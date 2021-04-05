e781d9b2eb56c54025ac8ec6a0caea075368aeed
bdfalumnos <bdfalumnos@gmail.com>
2021-04-05 12:52:16 -0500

    CD

--- js/CtrlAlumnos.js
+++ js/CtrlAlumnos.js
@@ -80,7 +80,7 @@ function htmlFila(doc) {
       <a class="fila" href=
   "alumno.html?${parámetros}">
         <strong class="primario">
-          ${nombre} ${espacio} ${dformat}
+          ${matricula} ${nombre} ${dformat}
         </strong>
       </a>
      
