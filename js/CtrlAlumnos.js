2574431d6b7ab5fb76ceb3003b985334e7f54401
bdfalumnos <bdfalumnos@gmail.com>
2021-04-05 12:47:58 -0500

    Ca

--- js/CtrlAlumnos.js
+++ js/CtrlAlumnos.js
@@ -70,7 +70,7 @@ function htmlFila(doc) {
   const nombre = cod(data.nombre);
   var fsf= cod(data.fecha);
   var fecha = new Date(fsf);
-  var espacio="      ";
+  var espacio="   --   ";
   var dformat = [fecha.getDate()+1, fecha.getMonth()+1, fecha.getFullYear()].join('/');
   const parámetros =
     new URLSearchParams();
