976e971f1c0a35331de94a0370900c5299d1effc
bdfalumnos <81336903+bdfalumnos@users.noreply.github.com>
2023-03-20 18:06:05 -0600

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -3,7 +3,7 @@ class MiFooter
   connectedCallback() {
     this.innerHTML = /* html */
       `<p>
-        &copy; 2021
+        &copy; 2023
         Ricardo Armando Machorro Reyes.
       </p>`;
   }
