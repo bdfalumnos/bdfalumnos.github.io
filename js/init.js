a1a2c88a44996381e112013c7af7a67eaf6ff6a7
bdfalumnos <81336903+bdfalumnos@users.noreply.github.com>
2023-11-15 11:23:29 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyDJC1FVauq1Oif4iVR5oZXdahKDgPcoRcg",
+    authDomain: "auborrar.firebaseapp.com",
+    projectId: "auborrar",
+    storageBucket: "auborrar.appspot.com",
+    messagingSenderId: "791156242736",
+    appId: "1:791156242736:web:4527226443aff6bf89b0f5",
+    measurementId: "G-2PQCVZ7L79"
 });
